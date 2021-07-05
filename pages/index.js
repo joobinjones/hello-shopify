3c19659488d7861063fa1e19f6910d42d3e0afa8
austinhjones3 <austinhjones3@gmail.com>
2021-07-05 18:10:43 -0500

    selector, add functionality, and cancel selector all working

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
-import { Heading, Page } from "@shopify/polaris";
+import React from "react";
+import { Page } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
+export default class Index extends React.Component {
+  state = { open: false };
+  render() {
+    return (
+      <Page
+        title="Product Selector"
+        primaryAction={{
+          content: "Select Products",
+          onAction: () => this.setState({ open: true }),
+        }}
+      >
+        <ResourcePicker
+          resourceType="Product"
+          open={this.state.open}
+          onCancel={() => this.setState({ open: false })}
+          // "resources" is the array of stuff the user selects and adds
+          onSelection={(resources) => this.handleSelection(resources)}
+        />
+      </Page>
+    );
+  }
 
-const Index = () => (
-  <Page
-    title="Product Selector"
-    primaryAction={{
-      content: "Select Products",
-      onAction: function () {
-        console.log("clicked");
-      },
-    }}
-  />
-);
-
-export default Index;
+  // resources are caught here in the handleSelection method and logged
+  handleSelection(resources) {
+    const idFromResources = resource.selection.map((product) => product.id);
+    this.setState({ open: false });
+    console.log(resources);
+    console.log(idFromResources);
+  }
+}
